407e23f8613101011cc1a76d94cd0eea1d0ea2ef
csaborio <christian@saborio.org>
2019-08-03 23:22:00 +1000

    ReportTable included, .gitignore file modified to exclude autoload.php

---
+++ src/ReportTable.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ScorpioTek\Reports;
+
+class ReportTable {
+    
+    /**
+     * __construct - main construct
+     *
+     * @param  string $template_name - the template name of the function to load.
+     *
+     * @return void
+     */
+    public function __construct( $template_name = 'default' ) {
+
+    }
+}
